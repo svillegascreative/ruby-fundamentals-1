05d9f1264dcec3bb9ea2f69bfaaec67920e77a5a
sylvia villegas <sylvia@svillegas.com>
2017-01-16 14:40:29 -0500

    Complete exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+# 1: Calcuate a good tip for $55 meal
+# fair:
+puts 55 * 1.13
+# better:
+puts 55 * 1.2
+
+# 2
+puts "I need " + 3.to_s + " coffees, " + 6.to_s + " horses, and like, " + 12.to_s + " million dollars"
+
+# 3
+puts "If I've told you once, I've told you #{45628 * 7839} times!"
+
+# 4: Predicted return is: true <---- And it was!
+puts (10 < 20 && 30 < 20) || !(10 == 11)
