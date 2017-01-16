7a63e0e471063eed19caa4781c44e764570606ac
sylvia villegas <sylvia@svillegas.com>
2017-01-16 15:03:42 -0500

    complete exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi, #{name}!"
+puts "How old are you?"
+age = gets.to_i
+puts "You were born in #{2016 - age}, unless you already had a birthday this year."
